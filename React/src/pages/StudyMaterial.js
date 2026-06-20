e41668a8dc5ffded1d53e016ed86faae75243c28
Pratik373 <pmdange99@gmail.com>
2026-06-20 17:46:01 +0530

    chore: remove unused useRef imports to fix CI build compilation warnings

--- React/src/pages/StudyMaterial.js
+++ React/src/pages/StudyMaterial.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState, useRef } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
 import ProfileDropdown from '../components/ProfileDropdown'
